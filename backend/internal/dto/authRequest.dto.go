070a6e95ec3015534136646a05cad8002d2c313f
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-08 01:07:46 +0700

    feat: add new field struct in auth register dto

--- backend/internal/dto/authRequest.dto.go
+++ backend/internal/dto/authRequest.dto.go
@@ -1,8 +1,9 @@
 package dto
 
 type AuthRegisterRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required"`
+	ConfirmPassword string `json:"confirmPassword" binding:"required"`
 }
 
 type AuthLoginRequest struct {
